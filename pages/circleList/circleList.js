4e628fdc8a05b3b6e1810c28c4d2c442ab88a6fe
L <327637616@qq.com>
2017-03-04 22:55:23 +0800

    nihao

--- pages/circleList/circleList.js
+++ pages/circleList/circleList.js
@@ -1,19 +1,35 @@
-// pages/circleList/circleList.js
+//index.js
+//获取应用实例
+var app = getApp();
+
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    circles: ['交友圈','相亲圈','二手交易圈','枪支圈','同性恋圈','资料圈']
   },
-  onReady:function(){
-    // 页面渲染完成
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-  onShow:function(){
-    // 页面显示
+  //创建选择的圈子
+  create:function(){
+    wx.navigateTo({
+      url: '../createCircle/createCircle'
+    })
   },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  onLoad: function () {
+    console.log('onLoad')
+    wx.setNavigationBarTitle({
+      title: '创建圈子'
+    })
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   }
 })
--- pages/createCircle/createCircle.js
+++ pages/createCircle/createCircle.js
@@ -1,33 +1,19 @@
-//index.js
-//获取应用实例
-var app = getApp();
-
+// pages/circleList/circleList.js
 Page({
-  data: {
-    circles: ['交友圈','相亲圈','二手交易圈','枪支圈','同性恋圈','资料圈']
+  data:{},
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onReady:function(){
+    // 页面渲染完成
   },
-  //创建选择的圈子
-  create:function(){
-    
+  onShow:function(){
+    // 页面显示
   },
-  onLoad: function () {
-    console.log('onLoad')
-    wx.setNavigationBarTitle({
-      title: '创建圈子'
-    })
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
   }
 })
